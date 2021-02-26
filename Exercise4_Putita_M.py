be25449640e9edf155e2180b90260032a542364e
Putitamos28 <79504360+Putitamos28@users.noreply.github.com>
2021-02-26 23:33:07 +0700

    Create Exercise4_Putita_M.py

---
+++ Exercise4_Putita_M.py
@@ -0,0 +1,9 @@
+F_Eng = 70.5
+G_Business = 75.5
+I_Com = 80.2
+C_Program = 85.5
+print("---Your Score---")
+print("Foundation English =", F_Eng)
+print("General Business =", G_Business)
+print("Introduction to Computer System =", I_Com)
+print("Computer Programming =", C_Program)
