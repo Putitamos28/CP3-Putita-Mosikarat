72869a2c88335d245a0d3a4ead0f0f482aeb6653
Putitamos28 <79504360+Putitamos28@users.noreply.github.com>
2021-05-02 16:56:34 +0700

    Create Lec50_Putita_M.py

---
+++ Lec50_Putita_M.py
@@ -0,0 +1,12 @@
+def addNumber(x,y):
+    print(x+y)
+def minuteNumber(x,y):
+    print(x-y)
+def multiNumber(x,y):
+    print(x*y)
+def divideNumber(x,y):
+    print(x/y)
+addNumber(100,10)
+multiNumber(100,10)
+multiNumber(100,10)
+divideNumber(100,10)
